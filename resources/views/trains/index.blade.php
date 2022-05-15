9df92b65a2453e74fd13cb695b883e0e0af6abf5
patriciabrsk <patriciabresku@gmail.com>
2022-05-15 21:12:34 +0200

    create trains migration

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -9,21 +9,19 @@
                 <h2 class="text-uppercase">Movies</h2>
             </div>
             <div class="row row-cols-4 g-4 text-center">
-                
+                @foreach ($trains as $train)
                 <div class="col">
                     <div class="card">
-                        <img src="" class="card-img-top img-fluid" alt="">
                         <div class="card-body">
-                            <h4 class="card-title"></h4>
-                            <h5 class="card-title"></h5>
-                            <p class="card-text">
-                                <strong>Nationality:</strong> <br>
-                                <strong>Release date:</strong> <br>
-                                <strong>Rating:</strong> <br>
-                            </p>
+                            <h5 class="card-title">{{ $train->azienda }} - {{ $train->codice_treno }}</h5>
+                            <p class="card-text">{{ $train->orario_di_partenza }} {{ $train->stazione_di_partenza }} - {{ $train->stazione_di_arrivo }} {{ $train->orario_di_arrivo }}</p>
+                            <p class="card-text">{{ $train->orario_di_partenza }} - {{ $train->orario_di_arrivo }}</p>
+                            <p class="card-text">{{ $train->numero_carrozze }}</p>
+                            <p class="card-text">{{ $train->in_orario ? 'In orario' : 'Cancellato' }}</p>
                         </div>
                     </div>
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
