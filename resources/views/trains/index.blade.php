73265f3c1cd47b9a5c14afdfc4d0d251ad2f2be6
patriciabrsk <patriciabresku@gmail.com>
2022-05-16 10:14:26 +0200

    edit index title

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container py-5">
         <div class="row">
             <div class="col-12">
-                <h2 class="text-uppercase">Movies</h2>
+                <h2 class="text-uppercase">Trains</h2>
             </div>
             <div class="row row-cols-4 g-4 text-center">
                 @foreach ($trains as $train)
